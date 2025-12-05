0d0a2ac91219d8300ca068f6a8ffe88527f1721c
Dasmir Coleman <dasmircoleman@gmail.com>
2025-12-05 17:36:40 -0500

    commented code revision

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -8,7 +8,7 @@ public class App {
 
         //did we pass in a username and password
         //if not, the application must die
-        if(args.length != 2){
+        if (args.length != 2) {
             //display a message to the user
             System.out.println("Application needs two args to run: A username and a password for the db");
             //exit the app due to failure because we dont have a username and password from the command line
@@ -19,48 +19,83 @@ public class App {
         String username = args[0];
         String password = args[1];
 
-        try(
-                //Create the connection(similar to opening mySQL workbench)
-                Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", username, password);
+        try (Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", username, password)) {
 
-                PreparedStatement q = c.prepareStatement("""
-                    
-                    SELECT
-                        ProductName
-                    FROM
-                        Products
-                    
-                    """);
-                ResultSet results = q.executeQuery();
-        ){
-            printResults(results);
+            while (true) {
 
-        }catch(SQLException e){
+                System.out.println("""
+                         What do you want to do?
+                             1) Display All Products
+                             2) Display All Customers
+                             0) Exit App                       \s
+                        \s""");
+
+                switch (scan.nextInt()) {
+                    case (1):
+                        displayAllProducts(c);
+                        break;
+                    case (2):
+                        displayAllCustomers(c);
+                        break;
+                    case (0):
+                        System.out.println("Bye");
+                        System.exit(0);
+                    default:
+                        System.out.println("Try Again");
+                }
+            }
+
+        } catch (SQLException e) {
             System.out.println("Something went wrong" + e);
         }
-    }
 
-    //This method will be used inside of the display methods to print results to the screen
-    public static void printResults(ResultSet r) throws SQLException {
 
-        //Get the meta data so we have access to the field names
-        ResultSetMetaData metaData = r.getMetaData();
+//
+//        //try(
+//                //Create the connection(similar to opening mySQL workbench)
+//                Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", username, password);
+//
+//                PreparedStatement q = c.prepareStatement("""
+//
+//                    SELECT
+//                        ProductName
+//                    FROM
+//                        Products
+//
+//                    """);
+//                ResultSet results = q.executeQuery();
+//        ){
+//            printResults(results);
+//
+//        }catch(SQLException e){
+//            System.out.println("Something went wrong" + e);
+//        }
+//    }
 
-        //Get the number of rows returned
-        int columnCount = metaData.getColumnCount();
+        //This method will be used inside of the display methods to print results to the screen
+        public static void printResults (ResultSet r) throws SQLException {
 
-        while(r.next()){
-            //loop over each column in the row and display the data
-            for(int i = 1; i <= columnCount; i++){
-                //Get the current column value
-                String value = r.getString(i);
-                //print out the column name and value
-                System.out.println(value);
+            //Get the meta data so we have access to the field names
+            ResultSetMetaData metaData = r.getMetaData();
+
+            //Get the number of rows returned
+            int columnCount = metaData.getColumnCount();
+
+            while (r.next()) {
+                //loop over each column in the row and display the data
+                int productID = r.getInt("ProductID");
+                String productName = r.getString("ProductName");
+                double unitPrice = r.getDouble("UnitPrice");
+                int unitsInStock = r.getInt("UnitsInStock");
+
+                System.out.printf("%-3d | %-35s | %-8.2f | %-10d", productID, productName, unitPrice, unitsInStock);
+//print empty line to make results display prettier
+                System.out.println();
+
+                //print empty line to make results display prettier
+                System.out.println();
             }
-            //print empty line to make results display prettier
-            System.out.println();
         }
-
     }
 }
 
